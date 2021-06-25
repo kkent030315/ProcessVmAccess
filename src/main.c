0094065a57e8e597c121d8c8207da6caac234fc7
kento <37926134+kkent030315@users.noreply.github.com>
2021-06-26 04:32:28 +0900

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,265 @@
+#include "main.h"
+
+#pragma warning(disable: 4047)
+#pragma warning(disable: 4100)
+
+NTSTATUS LookupProcessIdByProcessName(
+	_In_ PCWSTR ProcessName,
+	_Out_ PHANDLE UniqueProcessId)
+{
+	NTSTATUS ntStatus = STATUS_UNSUCCESSFUL;
+	PVOID Buffer;
+	UNICODE_STRING ProcessNameUs;
+
+	*UniqueProcessId = 0;
+
+	Buffer = ExAllocatePool(NonPagedPool, 1024 * 1024);
+
+	if (!Buffer)
+	{
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+	PSYSTEM_PROCESS_INFORMATION ProcessInformation = 
+		(PSYSTEM_PROCESS_INFORMATION)(Buffer);
+
+	ntStatus = ZwQuerySystemInformation(
+		SystemProcessInformation, ProcessInformation, 1024 * 1024, NULL);
+
+	if (!NT_SUCCESS(ntStatus))
+	{
+		ExFreePool(Buffer);
+		return ntStatus;
+	}
+
+	RtlInitUnicodeString(&ProcessNameUs, ProcessName);
+
+	for (;;)
+	{
+		if (RtlEqualUnicodeString(&ProcessInformation->ImageName, &ProcessNameUs, TRUE) == TRUE)
+		{
+			*UniqueProcessId = ProcessInformation->UniqueProcessId;
+
+			ExFreePool(Buffer);
+			return STATUS_SUCCESS;
+		}
+		else if (ProcessInformation->NextEntryOffset)
+		{
+			ProcessInformation = (PSYSTEM_PROCESS_INFORMATION)(
+				(PUCHAR)ProcessInformation + ProcessInformation->NextEntryOffset);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	ExFreePool(Buffer);
+
+	return STATUS_NOT_FOUND;
+}
+
+NTSTATUS CopyProcessVirtualMemory(
+	_In_ PEPROCESS Process,
+	_Inout_ PVOID Destination,
+	_Inout_ PVOID Source,
+	_In_ SIZE_T Size)
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+	KAPC_STATE ApcState;
+
+	// 1. Attach to the process
+	//    Sets specified process's PML4 to the CR3
+	KeStackAttachProcess((PRKPROCESS)Process, &ApcState);
+
+	__try
+	{
+		// 2. Copy specified user-process's virtua memory to 
+		//    the kernel's buffer
+		RtlCopyMemory(Destination, Source, Size);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		DbgPrint("[*] Exception occured: %lX\n", GetExceptionCode());
+		ntStatus = STATUS_UNSUCCESSFUL;
+	}
+
+	// 3. Detach from the process
+	//    Restores previous APC state to the current thread
+	KeUnstackDetachProcess(&ApcState);
+
+	return ntStatus;
+}
+
+NTSTATUS CopyProcessVirtualMemory2(
+	_In_ PEPROCESS Process,
+	_Inout_ PVOID Destination,
+	_Inout_ PVOID Source,
+	_In_ SIZE_T Size)
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+	KAPC_STATE ApcState;
+	PHYSICAL_ADDRESS SourcePhysicalAddress;
+	PVOID MappedIoSpace;
+	PVOID MappedKva;
+	PMDL Mdl;
+	BOOLEAN ShouldUseSourceAsUserVa;
+
+	ShouldUseSourceAsUserVa = Source <= MmHighestUserAddress ? TRUE : FALSE;
+
+	// 1. Attach to the process
+	//    Sets specified process's PML4 to the CR3
+	KeStackAttachProcess((PRKPROCESS)Process, &ApcState);
+
+	// 2. Get the physical address corresponding to the user virtual memory
+	SourcePhysicalAddress = MmGetPhysicalAddress(
+		ShouldUseSourceAsUserVa == TRUE ? Source : Destination);
+
+	// 3. Detach from the process
+	//    Restores previous APC state to the current thread
+	KeUnstackDetachProcess(&ApcState);
+
+	if (!SourcePhysicalAddress.QuadPart)
+	{
+		return STATUS_INVALID_ADDRESS;
+	}
+
+	DbgPrint("[+] Source Physical Address: 0x%llX\n", SourcePhysicalAddress.QuadPart);
+
+	// 4. Map an IO space for MDL
+	MappedIoSpace = MmMapIoSpace(SourcePhysicalAddress, Size, MmNonCached);
+
+	if (!MappedIoSpace)
+	{
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+	DbgPrint("[+] Physical Address 0x%llX is mapped to IO space 0x%p\n", 
+		SourcePhysicalAddress, MappedIoSpace);
+
+	// 5. Allocate MDL
+	Mdl = IoAllocateMdl(MappedIoSpace, (ULONG)Size, FALSE, FALSE, NULL);
+
+	if (!Mdl)
+	{
+		DbgPrint("[!] Failed to allocate MDL\n");
+		MmUnmapIoSpace(MappedIoSpace, Size);
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+	
+	// 6. Build MDL for non-paged pool
+	MmBuildMdlForNonPagedPool(Mdl);
+
+	// 7. Map to the KVA
+	MappedKva = MmMapLockedPagesSpecifyCache(
+		Mdl,
+		KernelMode,
+		MmNonCached,
+		NULL,
+		FALSE,
+		NormalPagePriority);
+
+	if (!MappedKva)
+	{
+		DbgPrint("[!] Failed to map physical pages\n");
+		MmUnmapIoSpace(MappedIoSpace, Size);
+		IoFreeMdl(Mdl);
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+	DbgPrint("[+] Mapped to KVA 0x%p\n", MappedKva);
+
+	// 8. copy memory
+	RtlCopyMemory(
+		ShouldUseSourceAsUserVa == TRUE ? Destination : MappedKva,
+		ShouldUseSourceAsUserVa == TRUE ? MappedKva : Destination,
+		Size);
+
+	MmUnmapIoSpace(MappedIoSpace, Size);
+	MmUnmapLockedPages(MappedKva, Mdl);
+	IoFreeMdl(Mdl);
+
+	return ntStatus;
+}
+
+NTSTATUS DispatchDriverEntry(
+	_In_ PDRIVER_OBJECT  DriverObject,
+	_In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+	HANDLE TargetProcessId;
+	PEPROCESS Process;
+	PVOID ProcessSectionBase;
+	USHORT PEMZ = NULL;
+
+	DbgPrint("[~] Looking for process \"notepad.exe\"");
+
+	if (!NT_SUCCESS(ntStatus = LookupProcessIdByProcessName(L"notepad.exe", &TargetProcessId)))
+	{
+		DbgPrint("[!] Failed to lookup process id\n");
+		return ntStatus;
+	}
+
+	// Possible overflow but ignore for this case
+	DbgPrint("[+] Target ProcessId: %d\n", TargetProcessId);
+
+	if (!NT_SUCCESS(ntStatus = PsLookupProcessByProcessId(TargetProcessId, &Process)))
+	{
+		DbgPrint("[!] Failed to lookup process\n");
+		return ntStatus;
+	}
+
+	// SectionBase should contain 0x5A4D (MZ) at start for valid PE
+	ProcessSectionBase = PsGetProcessSectionBaseAddress(Process);
+
+	DbgPrint("[+] PEPROCESS @ 0x%p\n", Process);
+	DbgPrint("[+] ProcessSectionBase: 0x%p\n", ProcessSectionBase);
+
+	if (!NT_SUCCESS(ntStatus = CopyProcessVirtualMemory(
+		Process, &PEMZ, ProcessSectionBase, sizeof(USHORT))))
+	{
+		DbgPrint("[!] Failed to copy process memory (1)\n");
+		return ntStatus;
+	}
+
+	DbgPrint("[+] Success (1): 0x%04X\n", PEMZ);
+
+	// Zero out the memory in order to perform again
+	RtlZeroMemory(&PEMZ, sizeof(PEMZ));
+
+	if (!NT_SUCCESS(ntStatus = CopyProcessVirtualMemory2(
+		Process, &PEMZ, ProcessSectionBase, sizeof(USHORT))))
+	{
+		DbgPrint("[!] Failed to copy process memory (2)\n");
+		return ntStatus;
+	}
+
+	DbgPrint("[+] Success (2): 0x%04X\n", PEMZ);
+
+	return ntStatus;
+}
+
+VOID
+DriverUnload(
+	IN PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	return;
+}
+
+NTSTATUS DriverEntry(
+	_In_ PDRIVER_OBJECT  DriverObject,
+	_In_ PUNICODE_STRING RegistryPath)
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+
+	DriverObject->DriverUnload = DriverUnload;
+
+	ntStatus = DispatchDriverEntry(DriverObject, RegistryPath);
+
+	return ntStatus;
+}
---
+++ src/main.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <ntifs.h>
+#include <ntddk.h>
+#include <intrin.h>
+
+NTKERNELAPI NTSTATUS ZwQuerySystemInformation(ULONG InfoClass, PVOID Buffer, ULONG Length, PULONG ReturnLength);
+NTKERNELAPI PVOID PsGetProcessSectionBaseAddress(_In_ PEPROCESS Process);
+
+typedef enum _SYSTEM_INFORMATION_CLASS
+{
+	SystemBasicInformation,
+	SystemProcessorInformation,
+	SystemPerformanceInformation,
+	SystemTimeOfDayInformation,
+	SystemPathInformation,
+	SystemProcessInformation,
+	SystemCallCountInformation,
+	SystemDeviceInformation,
+	SystemProcessorPerformanceInformation,
+	SystemFlagsInformation,
+	SystemCallTimeInformation,
+	SystemModuleInformation = 0x0B
+} SYSTEM_INFORMATION_CLASS, * PSYSTEM_INFORMATION_CLASS;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION
+{
+	ULONG NextEntryOffset;
+	ULONG NumberOfThreads;
+	LARGE_INTEGER SpareLi1;
+	LARGE_INTEGER SpareLi2;
+	LARGE_INTEGER SpareLi3;
+	LARGE_INTEGER CreateTime;
+	LARGE_INTEGER UserTime;
+	LARGE_INTEGER KernelTime;
+	UNICODE_STRING ImageName;
+	KPRIORITY BasePriority;
+	HANDLE UniqueProcessId;
+	HANDLE InheritedFromUniqueProcessId;
+	ULONG HandleCount;
+	ULONG SessionId;
+	ULONG_PTR PageDirectoryBase;
+	SIZE_T PeakVirtualSize;
+	SIZE_T VirtualSize;
+	ULONG PageFaultCount;
+	SIZE_T PeakWorkingSetSize;
+	SIZE_T WorkingSetSize;
+	SIZE_T QuotaPeakPagedPoolUsage;
+	SIZE_T QuotaPagedPoolUsage;
+	SIZE_T QuotaPeakNonPagedPoolUsage;
+	SIZE_T QuotaNonPagedPoolUsage;
+	SIZE_T PagefileUsage;
+	SIZE_T PeakPagefileUsage;
+	SIZE_T PrivatePageCount;
+	LARGE_INTEGER ReadOperationCount;
+	LARGE_INTEGER WriteOperationCount;
+	LARGE_INTEGER OtherOperationCount;
+	LARGE_INTEGER ReadTransferCount;
+	LARGE_INTEGER WriteTransferCount;
+	LARGE_INTEGER OtherTransferCount;
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
